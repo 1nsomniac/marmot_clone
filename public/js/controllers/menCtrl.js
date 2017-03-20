922816e0f0caf006a05c0a05874fc4768ab4b4ff
1nsomniac <samthakala@gmail.com>
2017-03-20 06:29:35 -0600

    getting a cors error...

--- public/js/controllers/menCtrl.js
+++ public/js/controllers/menCtrl.js
@@ -1,3 +1,14 @@
 angular.module('marmot').controller('menCtrl', function($scope, menSvc){
     console.log('found menCtrl')
+
+    function getProducts() {
+        menSvc.getProductsTwo().then(function (res) {
+            console.log(res)
+            $scope.mensProducts = res
+        })
+    }
+    getProducts()
+
+
+
 })
--- public/js/services/menSvc.js
+++ public/js/services/menSvc.js
@@ -1,3 +1,16 @@
 angular.module('marmot').service('menSvc', function($http){
     console.log('found menSvc')
+
+    this.getProducts = function () {
+        return $http.get('http://localhost:3000/')
+    }
+
+    this.getProductsTwo = function () {
+        return $http({
+            method: "GET",
+            url: 'http://localhost:3000/'
+        })
+    } 
+    
+
 })
